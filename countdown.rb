2db0a1c1c92da22efdd5e6a57c024a58b9d066f4
Landon <reasoner.landon90@gmail.com>
2020-07-09 22:47:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,13 +7,12 @@ while number_of_seconds > 0
 end
      "HAPPY NEW YEAR!"
 end
- 
+
 def countdown_with_sleep(number_of_seconds)
   while number_of_seconds > 0
     puts "#{number_of_seconds} SECOND(S)!"
     sleep (1)
     number_of_seconds -= 1
-  end 
+  end
   "HAPPY NEW YEAR!"
 end
-    
